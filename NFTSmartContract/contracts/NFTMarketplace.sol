b5e5064150764116babc459e99fdbca14ce8e250
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-17 16:39:07 +0300

    updated code

--- NFTSmartContract/contracts/NFTMarketplace.sol
+++ NFTSmartContract/contracts/NFTMarketplace.sol
@@ -5,3 +5,75 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    uint256 listingPrice = 0.00015 ether;
+
+    address payable owner;
+
+    mapping(uint256 => MarketItem) private idMarketItem;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }    
+
+    event idMarketItemCreated (
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner, 
+            "only owner of the marketplace can change the listing price"
+        );
+        _;
+    }
+
+    constructor() ERC721("Naiverse Token", "NAINFT") {
+        owner == payable(msg.sender);
+    }
+
+    function updateListingPrice(uint _listingPrice) public payable onlyOwner {
+
+        listingPrice = _listingPrice;
+
+    }
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    // LETS create "CREATE NFT TOKEN FUNCTION"
+
+    function createToken(string memory tokenURI, uint256 price) public payable returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newTokenId = _tokenIds.current();
+
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        createMarketItem(newTokenId, price);
+
+        return newTokenId;
+    }
+
+    //CREATING MARKET ITEMS
+
+    function createMarketItem() 
+}
