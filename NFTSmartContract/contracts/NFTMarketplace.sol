52331f7fc3f5e8134151064651365065129fa932
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-17 15:54:06 +0300

    updated code

---
+++ NFTSmartContract/contracts/NFTMarketplace.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+//INTERNAL IMPORT FOR NFT OPENZEPPELIN
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
