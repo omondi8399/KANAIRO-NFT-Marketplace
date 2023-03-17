db935615fabcb78ef70f56e0c955183140ebbc9c
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-17 18:58:30 +0300

    updated code

--- NFTSmartContract/contracts/NFTMarketplace.sol
+++ NFTSmartContract/contracts/NFTMarketplace.sol
@@ -123,5 +123,43 @@ contract NFTMarketplace is ERC721URIStorage {
         _transfer(address(this), msg.sender, tokenId);
 
         payable(owner).transfer(listingPrice);
+        payable(idMarketItem[tokenId].seller).transfer(msg.value);
+    }
+
+    //GETTING UNSOLD NFT DATA
+    function fetchMarketItem() public view returns(MarketItem[] memory) {
+        uint256 itemCount = _tokenIds.current();
+        uint256 unSoldItemCount = _tokenIds.current() - _itemsSold.current();
+        uint256 currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unSoldItemCount);
+        for (uint256 i = 0; i < itemCount; i++){
+            if(idMarketItem[i + 1].owner == address(this)) {
+                uint256 currentId = i + 1;
+
+                MarketItem storage currentItem = idMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
+
+    //PURCHASE ITEM
+    function fetchMyNFT() public view returns(MarketItem[] memory) {
+        uint256 totalCount = _tokenIds.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex = 0;
+
+        for (uint256 i = 0; i < totalCount; i++) {
+            if(idMarketItem[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }    
+        }
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for(uint256 i = 0; i < totalCount; i++) {
+
+            
+        }
     }
 }
