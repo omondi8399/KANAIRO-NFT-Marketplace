5b0f506a654d0395375e9ee7c40f338d4ecba85e
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-18 07:29:24 +0300

    updated code

--- NFTFrontEnd/components/NavBar/index.js
+++ NFTFrontEnd/components/NavBar/index.js
@@ -1,1 +1,8 @@
 import Discover from "./Discover/Discover"
+import HelpCenter from "./HelpCenter/HelpCenter"
+import Profile from "./Profile/Profile"
+import Notification from "./Notification/Notification"
+import Sidebar from "./Sidebar/Sidebar"
+
+
+export { Discover, HelpCenter, Profile, Notification, Sidebar }
