1199774ec4ed3b1a9b03cadf7dc1387cab00cae5
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-18 07:33:48 +0300

    updated code

--- NFTFrontEnd/components/NavBar/NavBar.jsx
+++ NFTFrontEnd/components/NavBar/NavBar.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import Image from "next/image"
+import Link from "next/link"
+
+//--------IMPORT ICON
+import {MdNotifications} from 'react-icons/md'
 
 //INTERNAL IMPORT
 import Style from "./NavBar.module.css"
