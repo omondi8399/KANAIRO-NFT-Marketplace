936d65a5c7de90b8d8977140b8ff6b51f6c3fd9a
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-19 08:45:02 +0300

    updated code

--- NFTFrontEnd/components/NavBar/Profile/Profile.jsx
+++ NFTFrontEnd/components/NavBar/Profile/Profile.jsx
@@ -1,8 +1,62 @@
 import React from 'react'
+import Image from "next/image"
+import { FaUserAlt, FaRegImage, FaUserEdit } from "react-icons/fa"
+import { MdHelpCenter } from "react-icons/md"
+import { TbDownloadOff, TbDownload } from "react-icons/tb"
+import Link from 'next/link'
+
+//INTERNAL IMPORT
+import Style from "./Profile.module.css"
+import images from "../../../img"
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className={Style.profile}>
+      <div className={Style.profile_account}>
+        <Image src={images.user1} alt="user profile" width={50} height={50} className={Style.profile_account_img} />
+        <div className={Style.profile_account_info}>
+          <p>Rodgers omondi</p>
+          <small>X09939494939904....</small>
+        </div>
+      </div>
+      <div className={Style.profile_menu}>
+        <div className={Style.profile_menu_one}>
+          <div className={Style.profile_menu_one_item}>
+            <FaUserAlt/>
+            <p>
+              <Link href={{pathname: '/myprofile'}}>My Profile</Link>
+            </p>
+          </div>
+          <div className={Style.profile_menu_one_item}>
+            <FaRegImage/>
+            <p>
+              <Link href={{pathname: '/my-item'}}>My Items</Link>
+            </p>
+          </div>
+          <div className={Style.profile_menu_one_item}>
+            <FaUserEdit/>
+            <p>
+              <Link href={{pathname: '/edit-profile'}}>Edit Profile</Link>
+            </p>
+          </div>
+        </div>
+
+        <div className={Style.profile_menu_two}>
+          <div className={Style.profile_menu_two_item}>
+            <MdHelpCenter/>
+            <p>
+              <Link href={{pathname: '/help'}}>Help</Link>
+            </p>
+          </div>
+          <div className={Style.profile_menu_two_item}>
+            <TbDownload/>
+            <p>
+              <Link href={{pathname: '/disconnect'}}>Disconnect</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
