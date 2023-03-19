1f23202fe9ffdb9a8ef4b2a817b51e7385636665
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-19 08:19:16 +0300

    updated code

--- NFTFrontEnd/components/NavBar/Notification/Notification.jsx
+++ NFTFrontEnd/components/NavBar/Notification/Notification.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Image from "next/image"
+
+// INTERNAL IMPORT 
+import Style from "./Notification.module.css"
+import images from "../../../img"
 
 const Notification = () => {
   return (
-    <div>Notification</div>
+    <div className={Style.notification}>
+      <P>Notification</P>
+      <div className={Style.notification_box}>
+        <div className={Style.notification_box_img}>
+          <Image src={images.user1} alt="profile image" width={50} height={50} className={Style.notification_box_img} />
+        </div>
+        <div className={Style.notification_box_info}>
+          <h4>Rodgers Omondi</h4>
+          <p>Measure action your user...</p>
+          <small>5 minutes</small>
+        </div>
+        <span className={Style.notification_box_new}></span>
+      </div>
+      </div>
   )
 }
 
