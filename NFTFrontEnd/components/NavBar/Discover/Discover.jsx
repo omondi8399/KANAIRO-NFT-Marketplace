b79fc249761808d4e42cefe0f67bd9f0bc39c92a
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-20 10:12:50 +0300

    Updated code

--- NFTFrontEnd/components/NavBar/Discover/Discover.jsx
+++ NFTFrontEnd/components/NavBar/Discover/Discover.jsx
@@ -1,51 +1,50 @@
-import React from 'react'
-import Link from "next/link"
+import React from "react";
+import Link from "next/link";
 
-// INTERNAL IMPORT
-import Style from "./Discover.module.css"
+//INTERNAL IMPORT
+import Style from "./Discover.module.css";
 
 const Discover = () => {
-
   //--------DISCOVER NAVIGATION MENU
   const discover = [
     {
       name: "Collection",
-      link: "collection"
+      link: "collection",
     },
     {
       name: "Search",
-      link: "search"
+      link: "search",
     },
     {
       name: "Author Profile",
-      link: "author-profile"
+      link: "author-profile",
     },
     {
       name: "NFT Details",
-      link: "NFT-details"
+      link: "NFT-details",
     },
     {
       name: "Account Setting",
-      link: "account-setting"
+      link: "account-setting",
     },
     {
       name: "Connect Wallet",
-      link: "connect-wallet"
+      link: "connect-wallet",
     },
     {
       name: "Blog",
-      link: "blog"
-    }
-    
-  ]
+      link: "blog",
+    },
+  ];
   return (
-    <div>{discover.map((el, i)=> (
-      <div key={i + 1} className={Style}>
-        <Link href={{ pathname: `${el.link}` }}>{el.name}</Link>
-      </div>
-    ))}
+    <div>
+      {discover.map((el, i) => (
+        <div key={i + 1} className={Style.discover}>
+          <Link href={{ pathname: `${el.link}` }}>{el.name}</Link>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Discover
+export default Discover;
