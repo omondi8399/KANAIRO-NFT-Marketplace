fc7d3c4d8c0dfbc30f57a855cdf80722ed5b9043
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-20 10:24:40 +0300

    Updated code

--- NFTFrontEnd/components/NavBar/Sidebar/Sidebar.jsx
+++ NFTFrontEnd/components/NavBar/Sidebar/Sidebar.jsx
@@ -1,62 +1,59 @@
-import React, { useState } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { GrClose } from "react-icons/gr"
-import { 
-  TiSocialFacebook, 
-  TiSocialLinkedin, 
-  TiSocialTwitter, 
-  TiSocialYoutube, 
-  TiSocialInstagram, 
-  TiArrowSortedDown, 
-  TiArrowSortedUp 
-      } from "react-icons/ti"
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { GrClose } from "react-icons/gr";
+import {
+  TiSocialFacebook,
+  TiSocialLinkedin,
+  TiSocialTwitter,
+  TiSocialYoutube,
+  TiSocialInstagram,
+  TiArrowSortedDown,
+  TiArrowSortedUp,
+} from "react-icons/ti";
 
+//INTERNAL IMPORT
+import Style from "./Sidebar.module.css";
+import images from "../../../img";
+import Button from "../../Button/Button";
 
-// INTERNAL IMPORT 
-import Style from "./Sidebar.module.css"
-import images from "../../../img"
-import Button from "../../Button/Button"
+const SideBar = ({ setOpenSideMenu }) => {
+  //------USESTATE
+  const [openDiscover, setOpenDiscover] = useState(false);
+  const [openHelp, setOpenHelp] = useState(false);
 
-const Sidebar = ({ setOpenSideMenu }) => {
-  //-------USESTATE
-  const [openDiscover,setOpenDiscover] = useState(false)
-  const [openHelp, setOpenHelp] = useState(false)
-
- //----------DISCOVER NAVIGATION MENU
+  //--------DISCOVER NAVIGATION MENU
   const discover = [
     {
       name: "Collection",
-      link: "collection"
+      link: "collection",
     },
     {
       name: "Search",
-      link: "search"
+      link: "search",
     },
     {
       name: "Author Profile",
-      link: "author-profile"
+      link: "author-profile",
     },
     {
       name: "NFT Details",
-      link: "NFT-details"
+      link: "NFT-details",
     },
     {
       name: "Account Setting",
-      link: "account-setting"
+      link: "account-setting",
     },
     {
       name: "Connect Wallet",
-      link: "connect-wallet"
+      link: "connect-wallet",
     },
     {
       name: "Blog",
-      link: "blog"
-    }
-    
-  ]
-
-  //---------HELP CENTER
+      link: "blog",
+    },
+  ];
+  //------HELP CNTEER
   const helpCenter = [
     {
       name: "About",
@@ -78,101 +75,108 @@ const Sidebar = ({ setOpenSideMenu }) => {
       name: "Subscription",
       link: "subscription",
     },
-  ]
+  ];
 
-  const openDiscoverMenu = ()=> {
-    if(!openDiscover) {
-      setOpenDiscover(true)
+  const openDiscoverMenu = () => {
+    if (!openDiscover) {
+      setOpenDiscover(true);
     } else {
-      setOpenDiscover(false)
+      setOpenDiscover(false);
     }
-  }
+  };
 
   const openHelpMenu = () => {
-    if(!openHelp) {
-      setOpenHelp(true)
+    if (!openHelp) {
+      setOpenHelp(true);
     } else {
-      setOpenHelp(false)
+      setOpenHelp(false);
     }
-  }
+  };
 
-  const closeSidebar = () => {
-    setOpenSideMenu(false)
-  }
+  const closeSideBar = () => {
+    setOpenSideMenu(false);
+  };
 
   return (
-    <div className={Style.sidebar}>
-      <GrClose className={Style.sidebar_closeBtn} onClick={()=> closeSidebar()} />
+    <div className={Style.sideBar}>
+      <GrClose
+        className={Style.sideBar_closeBtn}
+        onClick={() => closeSideBar()}
+      />
 
-      <div className={Style.sidebar_box}>
+      <div className={Style.sideBar_box}>
         <Image src={images.logo} alt="logo" width={150} height={150} />
-        <p>Discover the Most OutStanding articles on all pieces of NFT & your own stories and share them</p>
-        <div className={Style.sidebar_social}>
-          <a href='#'>
+        <p>
+          Discover the most outstanding articles on all topices of NFT & write
+          your own stories and share them
+        </p>
+        <div className={Style.sideBar_social}>
+          <a href="#">
             <TiSocialFacebook />
           </a>
-          <a href='#'>
+          <a href="#">
             <TiSocialLinkedin />
           </a>
-          <a href='#'>
+          <a href="#">
             <TiSocialTwitter />
           </a>
-          <a href='#'>
-            <TiSocialInstagram />
-          </a>
-          <a href='#'>
+          <a href="#">
             <TiSocialYoutube />
+          </a>
+          <a href="#">
+            <TiSocialInstagram />
           </a>
         </div>
       </div>
 
-      <div className={Style.sidebar_menu}>
+      <div className={Style.sideBar_menu}>
         <div>
-          <div className={Style.sidebar_menu_box} onClick={()=> openDiscoverMenu()}>
+          <div
+            className={Style.sideBar_menu_box}
+            onClick={() => openDiscoverMenu()}
+          >
             <p>Discover</p>
-            <TiArrowSortedDown/>
+            <TiArrowSortedDown />
           </div>
 
-          {
-            openDiscover && (
-              <div className={Style.sidebar_discover}>
-                {discover.map((el, i) => (
-                  <p key={i + 1}>
-                    <Link href={{ pathname: `${el.link}` }}>{el.name}</Link>
-                  </p>
-                ))}
-              </div>
-            )
-          }
+          {openDiscover && (
+            <div className={Style.sideBar_discover}>
+              {discover.map((el, i) => (
+                <p key={i + 1}>
+                  <Link href={{ pathname: `${el.link}` }}>{el.name}</Link>
+                </p>
+              ))}
+            </div>
+          )}
         </div>
-        <div>
-          <div className={Style.sidebar_menu_box} onClick={() => openHelpMenu()}>
 
+        <div>
+          <div
+            className={Style.sideBar_menu_box}
+            onClick={() => openHelpMenu()}
+          >
             <p>Help Center</p>
             <TiArrowSortedDown />
           </div>
 
-          {
-            openHelp && (
-              <div className={Style.sidebar_discover}>
-                {helpCenter.map((el, i)=> (
-                  <p key={i + 1}>
-                    <Link href={{ pathname: `${el.link}` }}>{el.name}</Link>
-                  </p>
-                ))}
-              </div>
-            )
-
-          }
+          {openHelp && (
+            <div className={Style.sideBar_discover}>
+              {helpCenter.map((el, i) => (
+                <p key={i + 1}>
+                  <Link href={{ pathname: `${el.link}` }}>{el.name}</Link>
+                </p>
+              ))}
+            </div>
+          )}
         </div>
       </div>
 
-      <div className={Style.sidebar_button}>
+      <div className={Style.sideBar_button}>
         <Button btnName="Create" handleClick={() => {}} />
         <Button btnName="Connect Wallet" handleClick={() => {}} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default SideBar;
