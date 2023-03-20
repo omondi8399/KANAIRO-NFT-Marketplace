7d5dbbdbef04fa7b5357185eb37261bb525a4c11
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-20 10:18:21 +0300

    Updated code

--- NFTFrontEnd/components/NavBar/HelpCenter/HelpCenter.jsx
+++ NFTFrontEnd/components/NavBar/HelpCenter/HelpCenter.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 //INTERNAL IMPORT
-import Style from "./Helpcenter.module.css"
+import Style from "./HelpCenter.module.css";
 
 const HelpCenter = () => {
-
   const helpCenter = [
     {
       name: "About",
@@ -27,16 +26,16 @@ const HelpCenter = () => {
       name: "Subscription",
       link: "subscription",
     },
-  ]
+  ];
   return (
     <div className={Style.box}>
-      {
-      helpCenter.map((el, i) => (
+      {helpCenter.map((el, i) => (
         <div className={Style.helpCenter}>
-          <Link href={{ pathname: `${el.link}`}}>{el.name}</Link>
-          </div>
-      ))}</div>
-  )
-}
+          <Link href={{ pathname: `${el.link}` }}>{el.name}</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default HelpCenter
+export default HelpCenter;
