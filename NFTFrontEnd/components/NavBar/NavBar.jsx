43db8c9fbdab5136df0b19b2797702bb94fba8e0
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-21 14:39:10 +0300

    Updated code

--- NFTFrontEnd/components/NavBar/NavBar.jsx
+++ NFTFrontEnd/components/NavBar/NavBar.jsx
@@ -16,8 +16,8 @@ const NavBar = () => {
   //----USESTATE COMPONNTS
   // const [discover, setDiscover] = useState(false);
   // const [help, setHelp] = useState(false);
-  // const [notification, setNotification] = useState(false);
-  // const [profile, setProfile] = useState(false);
+  const [notification, setNotification] = useState(false);
+  const [profile, setProfile] = useState(false);
   // const [openSideMenu, setOpenSideMenu] = useState(false);
 
   const openMenu = (e) => {
