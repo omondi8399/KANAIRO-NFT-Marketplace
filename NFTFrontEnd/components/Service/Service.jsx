c7f6bff2736ede6feabef8e8c46d7b6630687b80
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-22 18:53:55 +0300

    Updated code

--- NFTFrontEnd/components/Service/Service.jsx
+++ NFTFrontEnd/components/Service/Service.jsx
@@ -1,52 +1,80 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 //INTERNAL IMPORT
-import Style from "./Service.module.css"
-import images from "../../img"
-
+import Style from "./Service.module.css";
+import images from "../../img";
 const Service = () => {
   return (
     <div className={Style.service}>
       <div className={Style.service_box}>
         <div className={Style.service_box_item}>
-          <Image src={images.service1} alt="Filter & Discover" width={100} height={100}/>
+          <Image
+            src={images.service1}
+            alt="Filter & Discover"
+            width={100}
+            height={100}
+          />
           <p className={Style.service_box_item_step}>
             <span>Step 1</span>
           </p>
-          <h3> Filter & Discover</h3>
-          <p>Connect with wallet, discover, buy and sell your NFTSs and earn money </p>
+          <h3>Filter & Discover</h3>
+          <p>
+            Connect with wallet, discover, buy NTFs, sell your NFTs and earn
+            money
+          </p>
         </div>
-
         <div className={Style.service_box_item}>
-          <Image src={images.service2} alt="Filter & Discover" width={100} height={100}/>
+          <Image
+            src={images.service2}
+            alt="Filter & Discover"
+            width={100}
+            height={100}
+          />
           <p className={Style.service_box_item_step}>
             <span>Step 1</span>
           </p>
-          <h3> Filter & Discover</h3>
-          <p>Connect with wallet, discover, buy and sell your NFTSs and earn money </p>
+          <h3>Filter & Discover</h3>
+          <p>
+            Connect with wallet, discover, buy NTFs, sell your NFTs and earn
+            money
+          </p>
         </div>
-
         <div className={Style.service_box_item}>
-          <Image src={images.service3} alt="Filter & Discover" width={100} height={100}/>
+          <Image
+            src={images.service3}
+            alt="Connect Wallet"
+            width={100}
+            height={100}
+          />
           <p className={Style.service_box_item_step}>
             <span>Step 1</span>
           </p>
-          <h3> Connect Wallet</h3>
-          <p>Connect with wallet, discover, buy and sell your NFTSs and earn money </p>
+          <h3>Connect Wallet</h3>
+          <p>
+            Connect with wallet, discover, buy NTFs, sell your NFTs and earn
+            money
+          </p>
         </div>
-
         <div className={Style.service_box_item}>
-          <Image src={images.service4} alt="Filter & Discover" width={100} height={100}/>
+          <Image
+            src={images.service1}
+            alt="Filter & Discover"
+            width={100}
+            height={100}
+          />
           <p className={Style.service_box_item_step}>
             <span>Step 1</span>
           </p>
-          <h3> Filter & Discover</h3>
-          <p>Connect with wallet, discover, buy and sell your NFTSs and earn money </p>
+          <h3>Start trading</h3>
+          <p>
+            Connect with wallet, discover, buy NTFs, sell your NFTs and earn
+            money
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Service
+export default Service;
