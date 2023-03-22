f54b0983f23874160c1e5d6399ac14265226d000
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-22 16:03:23 +0300

    Updated code

--- NFTFrontEnd/components/Service/Service.jsx
+++ NFTFrontEnd/components/Service/Service.jsx
@@ -8,7 +8,43 @@ import images from "../../img"
 const Service = () => {
   return (
     <div className={Style.service}>
-      <div className={Style.service}
+      <div className={Style.service_box}>
+        <div className={Style.service_box_item}>
+          <Image src={images.service1} alt="Filter & Discover" width={100} height={100}/>
+          <p className={Style.service_box_item_step}>
+            <span>Step 1</span>
+          </p>
+          <h3> Filter & Discover</h3>
+          <p>Connect with wallet, discover, buy and sell your NFTSs and earn money </p>
+        </div>
+
+        <div className={Style.service_box_item}>
+          <Image src={images.service2} alt="Filter & Discover" width={100} height={100}/>
+          <p className={Style.service_box_item_step}>
+            <span>Step 1</span>
+          </p>
+          <h3> Filter & Discover</h3>
+          <p>Connect with wallet, discover, buy and sell your NFTSs and earn money </p>
+        </div>
+
+        <div className={Style.service_box_item}>
+          <Image src={images.service3} alt="Filter & Discover" width={100} height={100}/>
+          <p className={Style.service_box_item_step}>
+            <span>Step 1</span>
+          </p>
+          <h3> Connect Wallet</h3>
+          <p>Connect with wallet, discover, buy and sell your NFTSs and earn money </p>
+        </div>
+
+        <div className={Style.service_box_item}>
+          <Image src={images.service4} alt="Filter & Discover" width={100} height={100}/>
+          <p className={Style.service_box_item_step}>
+            <span>Step 1</span>
+          </p>
+          <h3> Filter & Discover</h3>
+          <p>Connect with wallet, discover, buy and sell your NFTSs and earn money </p>
+        </div>
+      </div>
     </div>
   )
 }
