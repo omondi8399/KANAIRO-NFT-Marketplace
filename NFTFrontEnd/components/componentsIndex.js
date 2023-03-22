d447062a68e68310efce8dce286b5eef3765ffed
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-22 10:20:11 +0300

    Updated code

--- NFTFrontEnd/components/componentsIndex.js
+++ NFTFrontEnd/components/componentsIndex.js
@@ -1,5 +1,6 @@
 import Button from "./Button/Button";
 import Footer from "./Footer/Footer";
 import NavBar from "./NavBar/NavBar";
+import HeroSection from "./HeroSection/HeroSection";
 
-export { Button, Footer, NavBar };
+export { Button, Footer, NavBar, HeroSection };
