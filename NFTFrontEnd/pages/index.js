aa5b106b5c7ba58feba4b6f7f78f2644b862fd58
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-22 10:25:16 +0300

    Updated code

--- NFTFrontEnd/pages/index.js
+++ NFTFrontEnd/pages/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Home= () => {
-  return <div>Home</div>;
+//INTERNAL IMPORT
+import Style from "../styles/index.module.css";
+import { HeroSection } from "../components/componentsIndex";
+
+const Home = () => {
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+    </div>
+  );
 };
 
 export default Home;
