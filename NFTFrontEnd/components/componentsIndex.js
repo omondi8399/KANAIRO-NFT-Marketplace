16aa48bfd1e921a3a4d9c7879e9d55eebd2aae29
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-23 15:46:51 +0300

    Updated code

--- NFTFrontEnd/components/componentsIndex.js
+++ NFTFrontEnd/components/componentsIndex.js
@@ -5,5 +5,6 @@ import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
 import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
 import Subscribe from "./Subscribe/Subscribe"
+import Title from "./Title/Title";
 
-export { Button, Footer, NavBar, HeroSection, Service, BigNFTSlider, Subscribe };
+export { Button, Footer, NavBar, HeroSection, Service, BigNFTSlider, Subscribe, Title };
--- NFTFrontEnd/pages/index.js
+++ NFTFrontEnd/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 //INTERNAL IMPORT
 import Style from "../styles/index.module.css";
-import { HeroSection, Service, BigNFTSlider,Subscribe } from "../components/componentsIndex";
+import { HeroSection, Service, BigNFTSlider,Subscribe, Title } from "../components/componentsIndex";
 
 const Home = () => {
   return (
@@ -11,6 +11,7 @@ const Home = () => {
       <Service />
       <BigNFTSlider />
       <Subscribe />
+      <Title heading="Browse by category" paragraph="Explore the NFTs in the most featured categories." />
     </div>
   );
 };
