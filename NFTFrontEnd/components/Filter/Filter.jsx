2b228c355b8c3ff5af9646a45d4ebb38124ffbcb
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-23 19:56:11 +0300

    Updated code

--- NFTFrontEnd/components/Filter/Filter.jsx
+++ NFTFrontEnd/components/Filter/Filter.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState }from 'react'
+import { 
+    FaFilter, 
+    FaAngleDown, 
+    FaAngleUp, 
+    FaWallet, 
+    FaMusic, 
+    FaVideo, 
+    FaImages, 
+    FaUserAlt
+} from "react-icons/fa"
+import { AiFillCloseCircle } from "react-icons/ai"
+import { MdVerified } from "react-icons/md"
+import { TiTick } from "react-icons/ti"
 
 //INTERNAL IMPORT 
 import Style from "./Filter.module.css"
 
 const Filter = () => {
+
+    
   return (
     <div>
       
