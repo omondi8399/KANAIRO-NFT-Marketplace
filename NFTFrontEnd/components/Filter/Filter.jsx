8a3805f452afe15fe7a1253996a60a84d7d3f714
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-23 20:31:45 +0300

    Updated code

--- NFTFrontEnd/components/Filter/Filter.jsx
+++ NFTFrontEnd/components/Filter/Filter.jsx
@@ -17,11 +17,44 @@ import { TiTick } from "react-icons/ti"
 import Style from "./Filter.module.css"
 
 const Filter = () => {
+    const [filter, setFilter] = useState(true)
+    const [image, setImage] = useState(true)
+    const [video, setVideo] = useState(true)
+    const [music, setMusic] = useState(true)
 
-    
   return (
-    <div>
-      
+    <div className={Style.filter}>
+      <div className={Style.filter_box}>
+        <div className={Style.filter_box_left}>
+          <button onClick={()=> {}}>NFTs</button>
+          <button onClick={()=> {}}>Arts</button>
+          <button onClick={()=> {}}>Music</button>
+          <button onClick={()=> {}}>Sports</button>
+          <button onClick={()=> {}}>Photography</button>
+        </div>
+
+        <div className={Style.filter_box_right}>
+          <div className={Style.filter_box_right_box} onClick={() => openFilter()}>
+            <FaFilter/>
+            <span>Filter</span> {filter ? <FaAngleDown /> : <FaAngleUp}
+          </div>
+        </div>
+      </div>
+
+      {
+        filter && (
+          <div className={Style.filter_box_items}>
+            <div className={Style.filter_box_items_box}>
+              <div className={Style.filter_box_items_box_item}>
+                <FaWallet /> <span>0.01 ETH - 10 ETH</span>
+                <AiFillCloseCircle />
+              </div>
+            </div>
+
+            <div className={Style}
+          </div>
+        )
+      }
     </div>
   )
 }
