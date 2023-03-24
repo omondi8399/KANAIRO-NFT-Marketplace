e179babd5484b7f4237518ad0662beb15c2c1593
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-24 09:57:40 +0300

    Updated code

--- NFTFrontEnd/components/NFTCard/NFTCard.jsx
+++ NFTFrontEnd/components/NFTCard/NFTCard.jsx
@@ -14,7 +14,36 @@ const NFTCard = () => {
   return (
     <div className={Style.NFTCard}>
         {featureArray.map((el, i)=>(
-            <div className={Style.NFTCard}
+            <div className={Style.NFTCard_box} key={i + 1}>
+                <div className={Style.NFTCard_box_img}>
+                    <Image src={images.nft_image_1} 
+                    alt="NFT images" 
+                    width={600} height={600}
+                    className={Style.NFTCard_box_img_img}/>
+                </div>
+
+                <div className={Style.NFTCard_box_update}>
+                    <div className={Style.NFTCard_box_update_left}>
+                        <div 
+                        className={Style.NFTCard_box_update_left_like}
+                        onClick={() => likeNFT()}>
+                        {like ? (
+                            <AiOutlineHeart />
+                        ) : (
+                        <AiFillHeart className={Style.NFTCard_box_update_left_like_icon}/>
+                        )}
+                        {""} 22
+                        </div>
+                    </div>
+
+                    <div className={Style.NFTCard_box_update_right}>
+                        <div className={Style.NFTCard_box_update_right_info}>
+                            <small>Remaining time</small>
+                            <p>3h : 15m 20s</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         ))}
     </div>
   )
