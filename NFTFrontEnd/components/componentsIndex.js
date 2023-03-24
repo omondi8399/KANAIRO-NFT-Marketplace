7d6f8caeb9ee3ec3913b02d0f6de4ba9130efb5c
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-24 19:23:53 +0300

    Updated code

--- NFTFrontEnd/components/componentsIndex.js
+++ NFTFrontEnd/components/componentsIndex.js
@@ -4,11 +4,24 @@ import NavBar from "./NavBar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
 import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
-import Subscribe from "./Subscribe/Subscribe"
+import Subscribe from "./Subscribe/Subscribe";
 import Title from "./Title/Title";
-import Category from "./Category/Category"
-import Filter from "./Filter/Filter"
-import NFTCard from "./NFTCard/NFTCard"
+import Category from "./Category/Category";
+import Filter from "./Filter/Filter";
+import NFTCard from "./NFTCard/NFTCard";
 import Collection from "./Collection/Collection";
 
-export { Button, Footer, NavBar, HeroSection, Service, BigNFTSlider, Subscribe, Title, Category, Filter, NFTCard, Collection};
+export {
+  Button,
+  Footer,
+  NavBar,
+  HeroSection,
+  Service,
+  BigNFTSlider,
+  Subscribe,
+  Title,
+  Category,
+  Filter,
+  NFTCard,
+  Collection,
+};
--- NFTFrontEnd/pages/index.js
+++ NFTFrontEnd/pages/index.js
@@ -2,7 +2,17 @@ import React from "react";
 
 //INTERNAL IMPORT
 import Style from "../styles/index.module.css";
-import { HeroSection, Service, BigNFTSlider,Subscribe, Title, Category, Filter, NFTCard, Collection } from "../components/componentsIndex";
+import {
+  HeroSection,
+  Service,
+  BigNFTSlider,
+  Subscribe,
+  Title,
+  Category,
+  Filter,
+  NFTCard,
+  Collection,
+} from "../components/componentsIndex";
 
 const Home = () => {
   return (
@@ -10,12 +20,21 @@ const Home = () => {
       <HeroSection />
       <Service />
       <BigNFTSlider />
-      <Title heading="New Collection" paragraph="Discover the most outstanding NFTs in all topics of life" />
+      <Title
+        heading="New Collection"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
       <Collection />
-      <Title heading="Features NFTs" paragraph="Discover the most outstanding NFTs in all topics of life" />
+      <Title
+        heading="Featured NFTs"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
       <Filter />
       <NFTCard />
-      <Title heading="Browse by category" paragraph="Explore the NFTs in the most featured categories." />
+      <Title
+        heading="Browse by category"
+        paragraph="Explore the NFTs in the most featured categories."
+      />
       <Category />
       <Subscribe />
     </div>
