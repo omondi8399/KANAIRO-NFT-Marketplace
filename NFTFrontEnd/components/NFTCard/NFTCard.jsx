a855abf246c869af745668994e460f3e5eecb923
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-24 19:14:12 +0300

    Updated code

--- NFTFrontEnd/components/NFTCard/NFTCard.jsx
+++ NFTFrontEnd/components/NFTCard/NFTCard.jsx
@@ -8,17 +8,7 @@ import Style from "./NFTCard.module.css";
 import images from "../../img";
 
 const NFTCard = () => {
-  const CardArray = [
-    images.nft_image_1,
-    images.nft_image_2,
-    images.nft_image_3,
-    images.nft_image_1,
-    images.nft_image_2,
-    images.nft_image_3,
-    images.nft_image_1,
-    images.nft_image_2,
-    images.nft_image_3,
-  ];
+  const featureArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   const [like, setLike] = useState(true);
 
@@ -32,11 +22,11 @@ const NFTCard = () => {
 
   return (
     <div className={Style.NFTCard}>
-      {CardArray.map((el, i) => (
+      {featureArray.map((el, i) => (
         <div className={Style.NFTCard_box} key={i + 1}>
           <div className={Style.NFTCard_box_img}>
             <Image
-              src={el}
+              src={images.nft_image_1}
               alt="NFT images"
               width={600}
               height={600}
