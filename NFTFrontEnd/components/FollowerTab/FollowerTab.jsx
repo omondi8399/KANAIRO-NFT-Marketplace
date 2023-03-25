61a7d339075348e17aa0170a5eb42975eb818e8d
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-25 10:48:52 +0300

    Updated code

--- NFTFrontEnd/components/FollowerTab/FollowerTab.jsx
+++ NFTFrontEnd/components/FollowerTab/FollowerTab.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { RiUserFollowFill, RiUserUnfollowFill, RiAwardLine, } from "react-icons/ri"
 
 //INTERNAL IMPORT 
 import Style from "./FollowerTab.module.css"
 
 const FollowerTab = () => {
-  return (
-    <div>
-       FollowerTab
+    const CardArray = [1,2,3,4,5,6,7,8]
+    const FollowingArray = [1,2,3,4,5,6]
+    const NewsArray = [1,2,3,4,5]
+
+    const [popular, setPopular] = useState(true)
+    const [following, setFollowing] = useState(false)
+    const [news, setNews] = useState(false)
+    return (
+    <div className={Style.followerTab}>
+        <div className={Style.followerTab.title}>
+            <h2> Top Creators List</h2>
+            <div className={Style.followerTab_tabs}>
+                <div className={Style.followerTab_tab_btn}>
+                    <button onClick={()=> openPopular()}>
+                        <RiUserFollowFill /> Popular
+                    </button>
+                    <button onClick={()=> openFollower()}>
+                        <RiUserFollowFill /> Following
+                    </button>
+                    <button onClick={()=> openNews()}>
+                        <RiAwardLine /> NoteWorthy
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        {
+            popular && (
+                <div className={Style.followerTab_box}>
+                    {CardArray.map((el, i)=> (
+                        <FollowerTabCard key={i + 1} i={i} el={el} />
+                    ))}
+                </div>
+            )
+        }
     </div>
-  )
+    )
 }
 
 export default FollowerTab
---
+++ NFTFrontEnd/components/FollowerTab/FollowerTabCard/FollowerTabCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//INTERNAL IMPORT
+import Style from "./FollowerTabCard.module.css"
+const FollowerTabCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FollowerTabCard
