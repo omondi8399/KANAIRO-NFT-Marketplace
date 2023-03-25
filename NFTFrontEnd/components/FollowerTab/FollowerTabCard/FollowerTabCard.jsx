0d88554279df8fe894d941d87eef55f09e46c470
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-25 13:40:04 +0300

    Updated code

--- NFTFrontEnd/components/FollowerTab/FollowerTabCard/FollowerTabCard.jsx
+++ NFTFrontEnd/components/FollowerTab/FollowerTabCard/FollowerTabCard.jsx
@@ -1,25 +1,24 @@
-import React, { useState } from 'react'
-import Image from 'next/image'
-import { MdVerified } from 'react-icons/md'
-import { TiTick } from "react-icons/ti"
-
+import React, { useState } from "react";
+import Image from "next/image";
+import { MdVerified } from "react-icons/md";
+import { TiTick } from "react-icons/ti";
 
 //INTERNAL IMPORT
-import Style from "./FollowerTabCard.module.css"
-import images from "../../../img"
-const FollowerTabCard = ({i, el}) => {
-  const [following, setFollowing] = useState(false)
-  const followMe = () => {
-    if (!followMe) {
-        setFollowing(true)
-    } else {
-      setFollowing(false)
-    }
-  }
+import Style from "./FollowerTabCard.module.css";
+import images from "../../../img";
+const FollowerTabCard = ({ i, el }) => {
+  const [following, setFollowing] = useState(false);
 
+  const followMe = () => {
+    if (!following) {
+      setFollowing(true);
+    } else {
+      setFollowing(false);
+    }
+  };
   return (
     <div className={Style.FollowerTabCard}>
-      <div className={Style.FollowerTab_rank}>
+      <div className={Style.FollowerTabCard_rank}>
         <p>
           #{i + 1} <span>🥇</span>
         </p>
@@ -27,49 +26,53 @@ const FollowerTabCard = ({i, el}) => {
 
       <div className={Style.FollowerTabCard_box}>
         <div className={Style.FollowerTabCard_box_img}>
-          <Image 
-          className={Style.FollowerTabCard_box_img_img}
-          src={images.creatorbackground1} 
-          alt="profile background"
-          width={500}
-          height={300}
-          objectFit="cover"
+          <Image
+            className={Style.FollowerTabCard_box_img_img}
+            src={images.creatorbackground1}
+            alt="profile braground"
+            width={500}
+            height={300}
+            objectFit="cover"
           />
         </div>
 
         <div className={Style.FollowerTabCard_box_profile}>
-          <Image 
-          className={Style.FollowerTabCard_box_profile_img} 
-          alt="profile picture" 
-          width={50} height={50}
-          src={images.user1}
+          <Image
+            className={Style.FollowerTabCard_box_profile_img}
+            alt="profile picture"
+            width={50}
+            height={50}
+            src={images.user1}
           />
         </div>
 
         <div className={Style.FollowerTabCard_box_info}>
           <div className={Style.FollowerTabCard_box_info_name}>
-            <h4>Henry Kash {""} {""} <span><MdVerified /></span> </h4>
+            <h4>
+              Henry Kash{""}{" "}
+              <span>
+                <MdVerified />
+              </span>
+            </h4>
             <p>12.321 ETH</p>
           </div>
 
           <div className={Style.FollowerTabCard_box_info_following}>
             {following ? (
               <a onClick={() => followMe()}>
-                Follow{""} {""}
+                Follow{""}{" "}
                 <span>
                   <TiTick />
                 </span>
               </a>
             ) : (
               <a onClick={() => followMe()}>Following</a>
-            )
-
-            }
+            )}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FollowerTabCard
+export default FollowerTabCard;
