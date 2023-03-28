dd15b22c7c10eeba2d185dd6eef1671fad23bfc4
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-28 08:17:39 +0300

    Updated code

---
+++ NFTFrontEnd/authorPage/authorNFTCardBox/authorNFTCardBox.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+
+//INTERNAL IMPORT
+import Style from "./AuthorNFTCardBox.module.css";
+import images from "../../img";
+import { NFTCardTwo } from "../../collectionPage/collectionIndex";
+import FollowerTabCard from "../../components/FollowerTab/FollowerTabCard/FollowerTabCard";
+
+const AuthorNFTCardBox = ({
+  collectiables,
+  created,
+  like,
+  follower,
+  following,
+}) => {
+  const collectiablesArray = [
+    images.nft_image_1,
+    images.nft_image_2,
+    images.nft_image_3,
+    images.nft_image_1,
+    images.nft_image_2,
+    images.nft_image_3,
+    images.nft_image_1,
+    images.nft_image_2,
+  ];
+
+  const createdArray = [
+    images.nft_image_1,
+    images.nft_image_2,
+    images.nft_image_3,
+    images.nft_image_1,
+  ];
+
+  const likeArray = [
+    images.nft_image_1,
+    images.nft_image_2,
+    images.nft_image_3,
+    images.nft_image_1,
+    images.nft_image_2,
+  ];
+
+  const followerArray = [
+    {
+      background: images.creatorbackground1,
+      user: images.user1,
+    },
+    {
+      background: images.creatorbackground2,
+      user: images.user2,
+    },
+    {
+      background: images.creatorbackground3,
+      user: images.user3,
+    },
+    {
+      background: images.creatorbackground4,
+      user: images.user4,
+    },
+    {
+      background: images.creatorbackground5,
+      user: images.user5,
+    },
+    {
+      background: images.creatorbackground6,
+      user: images.user6,
+    },
+  ];
+
+  const followingArray = [
+    {
+      background: images.creatorbackground3,
+      user: images.user3,
+    },
+    {
+      background: images.creatorbackground4,
+      user: images.user4,
+    },
+    {
+      background: images.creatorbackground5,
+      user: images.user5,
+    },
+    {
+      background: images.creatorbackground6,
+      user: images.user6,
+    },
+    {
+      background: images.creatorbackground1,
+      user: images.user1,
+    },
+  ];
+  return (
+    <div className={Style.AuthorNFTCardBox}>
+      {collectiables && <NFTCardTwo NFTData={collectiablesArray} />}
+      {created && <NFTCardTwo NFTData={createdArray} />}
+      {like && <NFTCardTwo NFTData={likeArray} />}
+      {follower && (
+        <div className={Style.AuthorNFTCardBox_box}>
+          {followerArray.map((el, i) => (
+            <FollowerTabCard i={i} el={el} />
+          ))}
+        </div>
+      )}
+      {following && (
+        <div className={Style.AuthorNFTCardBox_box}>
+          {followingArray.map((el, i) => (
+            <FollowerTabCard i={i} el={el} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AuthorNFTCardBox;
--- NFTFrontEnd/pages/author.js
+++ NFTFrontEnd/pages/author.js
@@ -1,35 +1,84 @@
-import React, { useState, useEffect} from 'react'
-
+import React, { useState, useEffect } from "react";
 
 //INTERNAL IMPORT
-import Style from "./styles/author.module.css"
-import { Banner, NFTCardTwo } from "../collectionPage/collectionIndex"
-import { Brand, Title } from "../components/componentsIndex"
-import images from "../img"
-// import { AuthorProfileCard, AuthorTaps}
-
+import Style from "../styles/author.module.css";
+import { Banner, NFTCardTwo } from "../collectionPage/collectionIndex";
+import { Brand, Title } from "../components/componentsIndex";
+import FollowerTabCard from "../components/FollowerTab/FollowerTabCard/FollowerTabCard";
+import images from "../img";
+import {
+  AuthorProfileCard,
+  AuthorTaps,
+  AuthorNFTCardBox,
+} from "../authorPage/componentIndex";
 
 const author = () => {
-        const popularArray = [
-          images.user1,
-          images.user2,
-          images.user3,
-          images.user4,
-          images.user5,
-          images.user6,
-          images.user7,
-          images.user8
-        ]
+  const followerArray = [
+    {
+      background: images.creatorbackground1,
+      user: images.user1,
+    },
+    {
+      background: images.creatorbackground2,
+      user: images.user2,
+    },
+    {
+      background: images.creatorbackground3,
+      user: images.user3,
+    },
+    {
+      background: images.creatorbackground4,
+      user: images.user4,
+    },
+    {
+      background: images.creatorbackground5,
+      user: images.user5,
+    },
+    {
+      background: images.creatorbackground6,
+      user: images.user6,
+    },
+  ];
+
+  const [collectiables, setCollectiables] = useState(true);
+  const [created, setCreated] = useState(false);
+  const [like, setLike] = useState(false);
+  const [follower, setFollower] = useState(false);
+  const [following, setFollowing] = useState(false);
+
   return (
-    <div>
-      
+    <div className={Style.author}>
+      <Banner bannerImage={images.creatorbackground2} />
+      <AuthorProfileCard />
+      <AuthorTaps
+        setCollectiables={setCollectiables}
+        setCreated={setCreated}
+        setLike={setLike}
+        setFollower={setFollower}
+        setFollowing={setFollowing}
+      />
+
+      <AuthorNFTCardBox
+        collectiables={collectiables}
+        created={created}
+        like={like}
+        follower={follower}
+        following={following}
+      />
+      <Title
+        heading="Popular Creators"
+        paragraph="Click on music icon and enjoy NTF music or audio
+"
+      />
+      <div className={Style.author_box}>
+        {followerArray.map((el, i) => (
+          <FollowerTabCard i={i} el={el} />
+        ))}
+      </div>
+
+      <Brand />
     </div>
-  )
-}
+  );
+};
 
-const [collectiables, setCollectiables] = useState(true)
-const [created, setcreated] = useState(false)
-const [like, setLike] = useState(false)
-
-
-export default author
+export default author;
