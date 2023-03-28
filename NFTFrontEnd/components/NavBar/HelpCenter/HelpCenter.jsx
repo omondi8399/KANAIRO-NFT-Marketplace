983ee0cab4a6ec3460d910945e21b3e1ccf8ee48
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-28 18:27:16 +0300

    Updated code

--- NFTFrontEnd/components/NavBar/HelpCenter/HelpCenter.jsx
+++ NFTFrontEnd/components/NavBar/HelpCenter/HelpCenter.jsx
@@ -8,7 +8,7 @@ const HelpCenter = () => {
   const helpCenter = [
     {
       name: "About",
-      link: "about",
+      link: "aboutus",
     },
     {
       name: "Contact Us",
---
+++ NFTFrontEnd/pages/aboutus.js
@@ -0,0 +1,114 @@
+import React from "react";
+import Image from "next/image";
+
+//INTERNAL IMPORT
+import Style from "../styles/aboutus.module.css";
+import { Brand } from "../components/componentsindex";
+import images from "../img";
+
+const aboutus = () => {
+  const founderArray = [
+    {
+      name: "Niamh O'Shea",
+      position: "Co-founder and Chief Executive",
+      images: images.founder1,
+    },
+    {
+      name: "Danien Jame",
+      position: "Co-founder and Chief Executive",
+      images: images.founder2,
+    },
+    {
+      name: "Orla Dwyer",
+      position: "Co-founder, Chairman",
+      images: images.founder3,
+    },
+    {
+      name: "Dara Frazier",
+      position: "Co-Founder, Chief Strategy Officer",
+      images: images.founder4,
+    },
+  ];
+
+  const factsArray = [
+    {
+      title: "10 million",
+      info: "Articles have been public around the world (as of Sept. 30, 2021)",
+    },
+    {
+      title: "100,000",
+      info: "Registered users account (as of Sept. 30, 2021)",
+    },
+    {
+      title: "220+",
+      info: "Countries and regions have our presence (as of Sept. 30, 2021",
+    },
+  ];
+  return (
+    <div className={Style.aboutus}>
+      <div className={Style.aboutus_box}>
+        <div className={Style.aboutus_box_hero}>
+          <div className={Style.aboutus_box_hero_left}>
+            <h1>👋 About Us.</h1>
+            <p>
+              We’re impartial and independent, and every day we create
+              distinctive, world-class programmes and content which inform,
+              educate and entertain millions of people in the around the world.
+            </p>
+          </div>
+          <div className={Style.aboutus_box_hero_right}>
+            <Image src={images.hero2} />
+          </div>
+        </div>
+
+        <div className={Style.aboutus_box_title}>
+          <h2>⛱ Founder</h2>
+          <p>
+            We’re impartial and independent, and every day we create
+            distinctive, world-class programmes and content
+          </p>
+        </div>
+
+        <div className={Style.aboutus_box_founder}>
+          <div className={Style.aboutus_box_founder_box}>
+            {founderArray.map((el, i) => (
+              <div className={Style.aboutus_box_founder_box_img}>
+                <Image
+                  src={el.images}
+                  alt={el.name}
+                  width={500}
+                  height={500}
+                  className={Style.aboutus_box_founder_box_img_img}
+                />
+                <h3>{el.name}</h3>
+                <p>{el.position}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className={Style.aboutus_box_title}>
+          <h2>🚀 Fast Facts</h2>
+          <p>
+            We’re impartial and independent, and every day we create
+            distinctive, world-class programmes and content
+          </p>
+        </div>
+
+        <div className={Style.aboutus_box_facts}>
+          <div className={Style.aboutus_box_facts_box}>
+            {factsArray.map((el, i) => (
+              <div className={Style.aboutus_box_facts_box_info}>
+                <h3>{el.title}</h3>
+                <p>{el.info}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <Brand />
+    </div>
+  );
+};
+
+export default aboutus;
