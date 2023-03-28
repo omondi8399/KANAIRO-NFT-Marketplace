3b76c6a613cf0d8b57873d0e06c3433d7acf96f9
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-28 05:08:45 +0300

    Updated code

--- NFTFrontEnd/pages/collection.js
+++ NFTFrontEnd/pages/collection.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
-
-//INTERNAL IMPORT 
-import Style from "../styles/collection.module.css"
-import images from "../img"
-import { Banner, CollectionProfile } from '../collectionPage/collectionIndex'
-import { Slider, Brand, NFTCard } from "../components/componentsIndex"
-import Filter from '../components/componentsIndex'
+//INTERNAL IMPORT
+import Style from "../styles/collection.module.css";
+import images from "../img";
+import {
+  Banner,
+  CollectionProfile,
+  NFTCardTwo,
+} from "../collectionPage/collectionIndex";
+import { Slider, Brand } from "../components/componentsIndex";
+import Filter from "../components/Filter/Filter";
 
 const collection = () => {
-
   const collectionArray = [
     images.nft_image_1,
     images.nft_image_2,
@@ -19,19 +21,18 @@ const collection = () => {
     images.nft_image_3,
     images.nft_image_1,
     images.nft_image_2,
-    images.nft_image_3,
-
-  ]
+  ];
   return (
     <div className={Style.collection}>
       <Banner bannerImage={images.creatorbackground1} />
       <CollectionProfile />
-      <NFTCard NFTData={collectionArray} />
       <Filter />
+      <NFTCardTwo NFTData={collectionArray} />
+
       <Slider />
       <Brand />
     </div>
-  )
-}
+  );
+};
 
-export default collection
+export default collection;
