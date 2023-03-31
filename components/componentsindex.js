69c1fa81ccc7e56bcf12c8aa3396647150adf261
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-31 18:38:40 +0300

    renamed file and folder

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -3,7 +3,7 @@ import Footer from "./Footer/Footer";
 import NavBar from "./NavBar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
-import BigNFTSilder from "./BigNFTSilder/BigNFTSilder";
+import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
 import Subscribe from "./Subscribe/Subscribe";
 import Title from "./Title/Title";
 import Category from "./Category/Category";
@@ -25,7 +25,7 @@ export {
   NavBar,
   HeroSection,
   Service,
-  BigNFTSilder,
+  BigNFTSlider,
   Subscribe,
   Title,
   Category,
