2320f68b28d18d4b73f16f871d65679cb64228e9
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-22 15:51:54 +0300

    Updated code

--- Context/NFTMarketplaceContext.js
+++ Context/NFTMarketplaceContext.js
@@ -57,7 +57,7 @@ const connectingWithSmartContract = async () => {
 export const NFTMarketplaceContext = React.createContext();
 
 export const NFTMarketplaceProvider = ({ children }) => {
-  const titleData = "Discover, collect, and sell NFTs";
+  const titleData = "KANAIRO NFT MARKETPLACE";
 
   //------USESTAT
   const [error, setError] = useState("");
